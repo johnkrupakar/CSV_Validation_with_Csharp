891d536d57c90338483f3a35cf40c57111da0b58
johnkrupakar <john245461@gmail.com>
2022-11-15 04:56:02 -0800

    updated with try and catch

--- Class1.cs
+++ Class1.cs
@@ -20,54 +20,62 @@ namespace CSVtoJSONLib
             //for storing unique valid accounts
             HashSet<String> validAccounts = new HashSet<String>();
 
-            if (File.Exists(dirPath))
+            try
             {
-                Console.WriteLine("File is Exist");
-                String[] lines = File.ReadAllLines(dirPath);
-                for (int i = 1; i < lines.Length; i++)
+                if (File.Exists(dirPath))
                 {
-                    String[] arr = lines[i].Split(',');
-                    if (arr[0] == "" || arr[1] == "" || arr[2] == "" || arr[3] == "" || arr[4] == "" || arr[5] == "")
+                    Console.WriteLine("File is Exist");
+                    String[] lines = File.ReadAllLines(dirPath);
+                    for (int i = 1; i < lines.Length; i++)
                     {
-                        continue;
-                    }
-                    else
-                    {
-                        if (validTransactions.Contains(arr[0]))
+                        String[] arr = lines[i].Split(',');
+                        if (arr[0] == "" || arr[1] == "" || arr[2] == "" || arr[3] == "" || arr[4] == "" || arr[5] == "")
                         {
                             continue;
                         }
                         else
                         {
-                            validTransactions.Add(arr[0]);
-                            Modal model = new Modal();
-                            model.AccountID = arr[3];
-                            model.AccountOwnerName = arr[4];
-                            model.AccountType = arr[5];
-                            model.TransactionID = Convert.ToInt32(arr[0]);
-                            model.TransactionDate = arr[1];
-                            model.TransactionAmout = Convert.ToDecimal(arr[2]);
+                            if (validTransactions.Contains(arr[0]))
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                validTransactions.Add(arr[0]);
+                                Modal model = new Modal();
+                                model.AccountID = arr[3];
+                                model.AccountOwnerName = arr[4];
+                                model.AccountType = arr[5];
+                                model.TransactionID = Convert.ToInt32(arr[0]);
+                                model.TransactionDate = arr[1];
+                                model.TransactionAmout = Convert.ToDecimal(arr[2]);
 
 
-                            string result = JsonConvert.SerializeObject(model, Formatting.Indented);
-                            //creating an storing the output in the Json file
-                            string filepath = @"D:\inputCSV.json";
-                            
-                            File.AppendAllText(filepath, result);
-                            Console.WriteLine(result);                        }
-                        //Because 1 account can have multiple transactions
-                        if (validAccounts.Contains(arr[3]))
-                        {
-                            continue;
-                        }
-                        else
-                        {
-                            validAccounts.Add(arr[3]);
+                                string result = JsonConvert.SerializeObject(model, Formatting.Indented);
+                                //creating an storing the output in the Json file
+                                string filepath = @"D:\inputCSV.json";
+
+                                File.AppendAllText(filepath, result);
+                                Console.WriteLine(result);
+                            }
+                            //Because 1 account can have multiple transactions
+                            if (validAccounts.Contains(arr[3]))
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                validAccounts.Add(arr[3]);
+                            }
                         }
                     }
                 }
             }
-            return ((validTransactions.Count()),(validAccounts.Count()));
+            catch (Exception ex)
+            {
+                Console.WriteLine("File is not Found at specified location");
+            }
+            return ((validTransactions.Count()), (validAccounts.Count()));
             Console.ReadLine();
         }
         public static void Main(String[] args)
@@ -75,6 +83,7 @@ namespace CSVtoJSONLib
             Console.WriteLine("Enter the Path of csv file: ");
             var dirPath = @"" + Console.ReadLine();
             Console.WriteLine(ProcessLines(dirPath));
+            Console.WriteLine("You have Successfully got your counts!!.");
             Console.ReadLine();
         }
     }
